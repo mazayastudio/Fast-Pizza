376946022fe315b25020415aad10d2c1a19420e5
sayudha <mazaya.studio@outlook.com>
2025-02-21 17:27:17 +0700

    Add Redux state management to handle user data
    Integrated Redux using @reduxjs/toolkit to manage user-related state. Updated the `Username` component to dynamically display the username from the Redux store, and wrapped the app in a `Provider` with a configured store. Added necessary dependencies for Redux and React-Redux.
    
    Signed-off-by: sayudha <mazaya.studio@outlook.com>

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,3 +1,6 @@
+import { getAddress } from '../../services/apiGeocoding.js';
+import { createSlice } from '@reduxjs/toolkit';
+
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -12,10 +15,29 @@ async function fetchAddress() {
     longitude: positionObj.coords.longitude,
   };
 
-  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+  // 2) Then we use a reverse geocoding API to get a description of the user's
+  // address, so we can display it the order form, so that the user can correct
+  // it if wrong
   const addressObj = await getAddress(position);
   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+
+const initialState = {
+  username: '',
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateName: (state, action) => {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+export default userSlice.reducer;
